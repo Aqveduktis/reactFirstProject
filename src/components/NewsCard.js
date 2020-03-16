624c420f451023035d6ea00de8ab2609a70c6171
Hassan Mian <hassanmian@hassans-imac.lan>
2020-03-16 14:48:51 +0100

    Add resize image effect

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useLayoutEffect, useState } from 'react';
 // import { Information } from 'components/Information'
 
+function useWindowSize() {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize([window.innerWidth, window.innerHeight]);
+      }
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  }
+
 export const NewsCard = (props) => {
+    const [width, height] = useWindowSize();
 
     return (
         <a href={props.anchor} className="news-card">
